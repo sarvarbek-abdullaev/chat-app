1c4ba8d7190794d2ac3bffa20391e8ccdbc9c7c5
sarvarbek-abdullaev <sarvar.bek.790@gmail.com>
2024-01-28 23:27:23 +0500

    feat: delete server

--- app/api/servers/[serverId]/route.ts
+++ app/api/servers/[serverId]/route.ts
@@ -2,6 +2,31 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { serverId: string } },
+) {
+  try {
+    const profile = await currentProfile();
+
+    const serverId = params.serverId;
+
+    if (!profile) return new Response("Unauthorized", { status: 401 });
+
+    const server = await db.server.delete({
+      where: {
+        id: serverId,
+        profileId: profile.id,
+      },
+    });
+
+    return NextResponse.json(server);
+  } catch (e) {
+    console.log("[SERVER_ID_DELETE]", e);
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { serverId: string } },
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -77,7 +77,10 @@ const ServerHeader: FC<ServerHeaderProps> = ({ server, role }) => {
         )}
         {isModerator && <DropdownMenuSeparator />}
         {isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("deleteServer", { server })}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
             Delete Server
             <Trash className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
